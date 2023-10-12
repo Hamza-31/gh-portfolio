7514981325c4d5335b5407239e2caee485e6cf6c
Hamza <hamza.labzioui@gmail.com>
2023-10-12 20:11:26 +0200

    Build dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from './github.service';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'gh-portfolio';
+export class AppComponent implements OnInit {
+	title = 'gh-portfolio';
+	username = '';
+	constructor(private gitubService: GithubService) { }
+	ngOnInit(): void {
+		this.username = this.gitubService.username;
+	}
 }
