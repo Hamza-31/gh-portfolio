8d4943546dfac31cbfa956fc3084e60c0863ef33
Hamza <hamza.labzioui@gmail.com>
2023-10-13 19:06:06 +0200

    Add head tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from './github.service';
-
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
 	selector: 'app-root',
@@ -10,8 +10,19 @@ import { GithubService } from './github.service';
 export class AppComponent implements OnInit {
 	title = 'gh-portfolio';
 	username = '';
-	constructor(private gitubService: GithubService) { }
+	constructor(private gitubService: GithubService, private titleService: Title, private meta: Meta) { }
 	ngOnInit(): void {
 		this.username = this.gitubService.username;
+		this.titleService.setTitle("Github portfolio app");
+		this.meta.addTags([
+			{
+				name: 'description',
+				content: `${this.username}'s Github portfolio`
+			},
+			{
+				name: 'author',
+				content: this.username
+			}
+		])
 	}
 }
